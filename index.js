200b42656c8b75de6678129e6a128593ca3b29f8
jongjin-j <jjjung3571@gmail.com>
2021-06-23 23:27:08 -0400

    Installed express

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import http from 'http'
+const express = require('express')
+const app = express()
 
-const app = http.createServer((req, res) => {
-   res.writeHead(200, {'Content-Type': 'text/plain'})
-   res.end('Hello World')
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!<h1>')
 })
 
 const PORT = 5000
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
 
