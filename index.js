e809f287f48649f475797caeb11ff38345592527
jongjin-j <jjjung3571@gmail.com>
2021-06-24 23:07:59 -0400

    Installed ejs

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const app = express()
 
+const PORT = 5000
+
+app.set('view engine', 'ejs')
+app.set('views', 'pages')
+
+const blogs = [
+  {title: 'Golf Course 1', location: 'Location 1'},
+  {title: 'Golf Course 2', location: 'Location 2'},
+  {title: 'Golf Course 3', location: 'Location 3'}
+]
+
+app.listen(PORT)
+
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!<h1>')
+  res.render('index', {title: 'Home', blogs})
 })
 
-const PORT = 5000
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.use((req, res) => {
+  res.status(404).render('error')
 })
 
