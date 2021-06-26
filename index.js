0ecd68e611f293e1884af4171623a0a691ddf302
jongjin-j <jjjung3571@gmail.com>
2021-06-26 00:41:50 -0400

    Added handling http requests

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
-const { render } = require('ejs')
+const blogRoutes = require('./routes/blogRoutes')
 
 const app = express()
 
@@ -21,52 +20,11 @@ app.use(morgan('dev'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('style'))
 
-const blogs = [
-  {title: 'Golf Course 1', location: 'Location 1'},
-  {title: 'Golf Course 2', location: 'Location 2'},
-  {title: 'Golf Course 3', location: 'Location 3'}
-]
-
 app.get('/', (req, res) => {
   res.redirect('/blogs')
 })
 
-app.get('/blogs', (req, res) => {
-  Blog.find().sort({ createdAt: -1 })
-    .then((result) => {
-      res.render('index', {title: 'All Blogs', blogs: result})
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-})
-
-app.post('/blogs', (req, res) => {
-  const blog = new Blog(req.body)
-  
-  blog.save()
-    .then((result) => {
-      res.redirect('/blogs')
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-})
-
-app.get('/blogs/new', (req, res) => {
-  res.render('create', {title: 'New Blog'})
-})
-
-app.get('/blogs/:id', (req, res) => {
-  const id = req.params.id
-  Blog.findById(id)
-    .then((result) => {
-      res.render('details', {blog: result, title: 'Blog Details'})
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-})
+app.use('/blogs', blogRoutes)
 
 app.use((req, res) => {
   res.status(404).render('error', {title: 'Error'})
